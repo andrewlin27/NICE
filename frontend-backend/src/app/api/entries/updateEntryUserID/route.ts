643a4537ca5d75f445a46f1a0bc8f4d82d9edd19
ermcgonagle <ermcgonagle>
2025-02-27 16:30:26 -0600

    api route added to update a given entry's user_id

---
+++ frontend-backend/src/app/api/entries/updateEntryUserID/route.ts
@@ -0,0 +1,32 @@
+import { createClientServiceRoleKey } from '../../../../utils/supabase/server';
+import { NextResponse } from 'next/server';
+
+export async function PUT(request: Request) {
+    try {
+        const supabase = await createClientServiceRoleKey();
+        const { firstName, userId } = await request.json();
+
+        if (!firstName) {
+            return NextResponse.json({ error: 'Missing id for update' }, { status: 400 });
+        }
+        if (!userId) {
+            return NextResponse.json({ error: 'Missing user id for update' }, { status: 400 });
+        }
+
+        console.log(`Updating record with first_name = ${firstName} to set user_id = ${userId}`);
+
+        const { data, error } = await supabase
+            .from("entries")
+            .update({"user_id": userId})
+            .ilike("first_name", firstName)
+            .select();
+
+        if (error) {
+            return NextResponse.json({ error: error.message }, { status: 500 });
+        }
+
+        return NextResponse.json(data, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
