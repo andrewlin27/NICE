faf367f053bd8e2551c9887833fb9bacb6501bfc
wu814 <william14.wu@gmail.com>
2025-03-04 17:08:06 -0600

    saving progress

---
+++ frontend-backend/src/app/api/entries/getEntryByID/[entryID]/route.ts
@@ -0,0 +1,34 @@
+import { createClientAnonKey, createClientServiceRoleKey } from '../../../../../utils/supabase/server';
+import { NextRequest, NextResponse } from 'next/server';
+
+// context object is where dynamic route parameters (params) are passed
+export async function GET(req: NextRequest, context: { params: { entryID: string } }) {
+    try{
+        const supabase = await createClientAnonKey();
+        const { entryID } = await context.params;
+
+        if (!entryID) {
+            return NextResponse.json({ error: 'Missing entry_id' }, { status: 400 });
+        }
+        
+        const { data: entry, error }: { data: any; error: any } = await supabase
+            .from('entries')
+            .select('*')
+            .eq('entry_id', entryID);
+        
+        console.log(entry);
+        if (entry.length === 0) {
+            return NextResponse.json({ error: 'Entry not found' }, { status: 404 });
+        }
+
+        if (error) {
+            return NextResponse.json({ error: error.message}, { status: 500 });
+        }
+
+        return NextResponse.json(entry, { status: 200 });
+    
+    } catch (error: any) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+     
+}
--- frontend-backend/src/app/entry/[entryId]/page.tsx
+++ frontend-backend/src/app/entry/[entryId]/page.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 
-const page = async ({ params }: { params: { entryId: string } }) => {
+const page = async ({ params }: { params: { entryID: string } }) => {
   // Fetch data from the API using the entryId from params
-  const entryId = params?.entryId;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/entries/getEntryByID/${entryId}`);
-  const data = await response.json(); // Assuming the API returns JSON
+  const prop = await params;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/entries/getEntryByID/${prop.entryID}`);
+  const data = await response.json(); 
 
   if (!response.ok) {
-    return <div>Error fetching data</div>;
+    return <div>Failed to load entry</div>;
   }
 
   return (
     <div>
-      <h1>Entry ID: {entryId}</h1>
+      <h1>Entry ID: {prop.entryID}</h1>
       <div>
-        {/* Render the fetched data here */}
         <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
     </div>
