85bfbf6d4ea1c259be52227075eebc7cb9a37305
ermcgonagle <ermcgonagle>
2025-03-19 15:51:56 -0500

    added line to sort entries by last name on entries page

--- frontend-backend/src/app/api/entries/getAllEntries/route.ts
+++ frontend-backend/src/app/api/entries/getAllEntries/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const supabase = await createClientAnonKey();
-    const { data: entries, error } = await supabase.from("entries").select('*');
+    const { data: entries, error } = await supabase.from("entries").select('*').order("last_name", { ascending: true });
 
     if (error)  {
       return NextResponse.json({ error: error.message }, { status: 500 });
