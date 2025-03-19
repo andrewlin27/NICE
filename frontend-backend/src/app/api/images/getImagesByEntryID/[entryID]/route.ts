438b41c6d275b3040d41bf04ae1406afa88234c1
wu814 <william14.wu@gmail.com>
2025-03-19 16:51:52 -0500

    huge success

---
+++ frontend-backend/src/app/api/images/getImagesByEntryID/[entryID]/route.ts
@@ -0,0 +1,33 @@
+import { createClientAnonKey, createClientServiceRoleKey } from '../../../../../utils/supabase/server';
+import { NextRequest, NextResponse } from 'next/server';
+
+// context object is where dynamic route parameters (params) are passed
+export async function GET(req: NextRequest, context: { params: { entryID: string } }) {
+    try{
+        const supabase = await createClientAnonKey();
+        const { entryID } = await context.params;
+
+        if (!entryID) {
+            return NextResponse.json({ error: 'Missing Entry ID' }, { status: 400 });
+        }
+        
+        const { data: image_links, error }: { data: any; error: any } = await supabase
+            .from('images')
+            .select('image_link')
+            .eq('entry_id', entryID);
+        
+        if (image_links.length === 0) {
+            return NextResponse.json({ error: `Entry ID ${entryID} not found` }, { status: 404 });
+        }
+
+        if (error) {
+            return NextResponse.json({ error: error.message}, { status: 500 });
+        }
+
+        return NextResponse.json(image_links, { status: 200 });
+    
+    } catch (error: any) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+     
+}
