ba2566bb32cf030632698f9701a73eedd2606d95
wu814 <william14.wu@gmail.com>
2025-03-25 17:09:46 -0500

    added add entry button

--- frontend-backend/src/app/entry/page.tsx
+++ frontend-backend/src/app/entry/page.tsx
@@ -1,9 +1,18 @@
 import SearchResults from "@/components/SearchResults";
+import AddEntryBtn from "@/components/AddEntryBtn";
 
 const EntryPage = () => {
     return (
         <div className="min-h-screen bg-gray-100 p-6">
-            <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">Entries</h1>
+           <div className="flex justify-between items-center mt-3 mb-5 relative">
+                <h1 className="text-3xl font-bold text-center text-gray-900 mb-6 absolute left-1/2 transform -translate-x-1/2">
+                    Entries
+                </h1>
+                <div className="ml-auto">
+                    <AddEntryBtn />
+                </div>
+            </div>
+            
             <SearchResults />
         </div>
     );
