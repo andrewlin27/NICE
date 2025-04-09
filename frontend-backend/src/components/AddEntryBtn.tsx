8dc7375a5b4e6d51ca6cb0694ccff1458c447532
Andrew Lin <andrew.mlin27@gmail.com>
2025-04-09 15:25:00 -0500

    fix error for stroke on icons

--- frontend-backend/src/components/AddEntryBtn.tsx
+++ frontend-backend/src/components/AddEntryBtn.tsx
@@ -80,7 +80,7 @@ const AddEntryBtn: React.FC = () => {
     <div>
       <Button variant="primary" onClick={() => setIsOpen(true)} className="ml-4 flex items-center justify-center space-x-2">
         <svg className="w-7 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12h4m-2 2v-4M4 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12h4m-2 2v-4M4 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
         </svg>
         <span>Add Entry</span>
 
