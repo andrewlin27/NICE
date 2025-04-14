639e7ae6556291f561e16257e4c39b73d6ff3f36
wu814 <william14.wu@gmail.com>
2025-04-14 16:07:30 -0500

    added trashcan icon

--- frontend-backend/src/components/Images.tsx
+++ frontend-backend/src/components/Images.tsx
@@ -157,7 +157,7 @@ export default function Images({ entryID }: { entryID: string }) {
             <div className="flex flex-wrap justify-center items-center gap-8">
                 {images.length > 0 ? (
                     images.map((img, index) => (
-                        <div className="flex flex-col items-center max-w-lg bg-white p-6 rounded-lg shadow-md text-center" key={index}>
+                        <div className="flex flex-col relative items-center max-w-lg bg-white p-6 rounded-lg shadow-md text-center" key={index}>
                             <img
                                 src={img.image_link}
                                 alt={`Scan ${index + 1}`}
@@ -165,9 +165,12 @@ export default function Images({ entryID }: { entryID: string }) {
                             />
                             <button
                                 onClick={() => handleDeleteImage(img.image_id)}
-                                className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors duration-200"
+                                className="absolute top-0 right-0 text-white px-4 py-2 rounded transition duration-300 ease-in-out hover:scale-110"
                             >
-                                Delete Image
+                                <svg className="w-6 h-6 text-red-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                </svg>
+
                             </button>
                             <p className="mt-2 text-lg text-gray-600 underline font-bold">Confidence Levels</p>
                             <p className="mt-0 text-lg text-gray-600">Glioma: {reports[img.image_link]?.results.confidence_glioma ?? "Loading..."}</p>
