de9fa60d3d742e7f78f958626532916fa0b07318
ermcgonagle <ermcgonagle>
2025-04-16 15:01:53 -0500

    initial logic for deletin gmultipole images

--- frontend-backend/src/app/api/images/removeImageByImageID/[...imageID]/route.ts
+++ frontend-backend/src/app/api/images/removeImageByImageID/[...imageID]/route.ts
@@ -1,21 +1,29 @@
 import { createClientServiceRoleKey } from '@//utils/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function DELETE(req: NextRequest, context: { params: { imageID: number } }) {
+export async function DELETE(req: NextApiRequest, context: { params: { imageID: number } }) {
   try {
     const supabase = await createClientServiceRoleKey();
-    const { imageID } = context.params;
+    // const { imageID } = context.params;
+    const { ids } = req.query;
+    const idStrings = Array.isArray(ids) ? ids : [ids];
 
-    if (!imageID) {
-      return NextResponse.json({ error: 'Missing Image ID' }, { status: 400 });
+    const imageIds = idStrings.map((id) => parseInt(id, 10)).filter((id): id is number => !isNaN(id));
+
+    if (imageIds.length === 0) {
+      return NextResponse.json({ error: 'Invalid image IDs' }, { status: 400 });
     }
+
+    // if (!imageID) {
+    //   return NextResponse.json({ error: 'Missing Image ID' }, { status: 400 });
+    // }
 
     // delete from storage
     // get the image link by imageID from the images table
     const { data: imageData, error: fetchError } = await supabase
       .from('images')
       .select('image_link')
-      .eq('image_id', imageID)
+      .eq('image_id', imageIds)
       .single();
 
     if (fetchError) {
@@ -37,7 +45,7 @@ export async function DELETE(req: NextRequest, context: { params: { imageID: num
     const { error: deleteError } = await supabase
       .from('images')
       .delete()
-      .eq('image_id', imageID);
+      .eq('image_id', imageIds);
 
     if (deleteError) {
       return NextResponse.json({ error: deleteError.message }, { status: 500 });
